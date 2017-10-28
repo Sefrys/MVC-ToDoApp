d1b4194719932e2f2e4ee71b9efc65fa09b7a5d3
Irek Zagan <sefrys@gmail.com>
2017-10-28 18:53:48 +0200

    Add methods for creating items

--- to_do_item.py
+++ to_do_item.py
@@ -5,5 +5,29 @@ class ToDoItem():
         self.description = description
         self.is_done = is_done
 
+    def item_name():
+        setting_name = True
+        while setting_name:
+            name = input("Name: ")
+            if len(name) > 20:
+                print("Name exceeed maximum characters (20)")
+                continue
+            return name
+
+    def item_description():
+        setting_description = True
+        while setting_description:
+            description = input("Description: ")
+            if len(description) > 150:
+                print("Description exceeds maximum characters (150)")
+                continue
+            return description
+
+    def add_new_item(self):
+        name = self.item_name()
+        description = self.item_description()
+        new_item = ToDoItem(name, description)
+        return new_item
+
     def __str__(self):
         return "Name: {}".format(self.name)
