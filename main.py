ed5123a16590ff0fabd34e782aadb8a1606d0932
Irek Zagan <sefrys@gmail.com>
2017-10-30 01:13:29 +0100

    Add console clearing

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os
 import sys
+import time
 from to_do_list import *
 
+def clear():
+    """Clear the display"""
+    os.system("cls" if os.name == "nt" else "clear")
 
 def main():
     menu_choices = """
@@ -16,25 +20,31 @@ def main():
     to_do_list = []
     in_menu = True
     while in_menu:
-        os.system("clear")
         print(menu_choices)
         menu_choice = input("Select an option: ")
         if menu_choice == "1":
+            clear()
             print("Add new item: \n")
             add_item_to_list(to_do_list)
+            clear()
         elif menu_choice == "2":
+            clear()
             display_list(to_do_list)
         elif menu_choice == "3":
+            clear()
             display_item_details(to_do_list)
         elif menu_choice == "4":
+            clear()
             modify_item(to_do_list)
         elif menu_choice == "5":
+            clear()
             delete_item(to_do_list)
         elif menu_choice == "6":
             sys.exit()
         else:
+            clear()
             print("Wrong input!")
-
+            time.sleep(3)
 
 if __name__ == "__main__":
     main()
